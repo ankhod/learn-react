fbc9c8aaae7d432c73c1287608200c1bb9d08bf8
ankhod <ankhod@ukr.net>
2025-03-18 15:24:55 +0200

    button

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function MyButton() {
+  return (
+    <button>
+      Я кнопка
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Добро пожаловать в моё приложение</h1>
+      <MyButton />
+    </div>
+  );
+}
+
